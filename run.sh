07ad89b1e8e213a9d576f250aa54bd373abefa85
G Gordon Worley III <gworley3@gmail.com>
2017-05-12 21:46:35 +0000

    adstage/adstage-deploy#166 catch term and send int because node is dumb

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -e
-exec bin/cli.js --enableServer 1
+term_handler() {
+  kill -2 $PID || kill -15 $PID || sudo kill -9 $PID || true
+}
+
+trap term_handler TERM INT
+bin/cli.js --enableServer 1 &
+PID=$!
+wait $!
